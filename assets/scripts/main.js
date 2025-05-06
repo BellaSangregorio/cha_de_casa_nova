745206c03d1335d7d01080204ee671e8fe745f07
guilherme-smarquees <guilhermesmarquees@MacBook-Air-de-Guilherme-2.local>
2025-05-06 20:58:19 -0300

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,50 @@
+const products =  [
+    {price: 10.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
+    {price: 20.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
+    {price: 30.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
+    {price: 40.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
+    {price: 50.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
+    {price: 60.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
+]
+
+async function getData(){
+    const data = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1s6AqgQhT1L7LUftV26jGVg6DpGo8IW6uzVOG9NMVpOc')
+    
+    console.log(await data.json())
+}
+
+getData()
+
+products.forEach(element => {
+    const newProd = document.createElement('div');
+    newProd.className = "product-container"
+    
+    const Prod = document.createElement('div');
+    Prod.className = "product"
+    
+    const img = document.createElement('img');
+    img.src = element.img
+    
+    const description = document.createElement('p');
+    description.className = "description"
+    description.textContent = element.description
+
+    const price = document.createElement('p');
+    price.className = "price-tag"
+    price.textContent = "R$ " + element.price
+    
+    const button = document.createElement('button');
+    button.textContent = "Presentear"
+    button.addEventListener('click', (event) => {
+        window.location.replace(`https://wa.me//5521971564253?text=Gostaria de presentear você com ${element.description} que selecionei no seu sites de presentes`)
+    })
+
+    newProd.appendChild(Prod)
+    Prod.appendChild(img)
+    Prod.appendChild(description)
+    Prod.appendChild(price)
+    Prod.appendChild(button)
+    document.querySelector('.main-content').appendChild(newProd)
+})
+
+
