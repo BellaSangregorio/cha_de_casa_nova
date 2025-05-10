1100cb22f3b94278b04f3f5dd7d41520cff2a408
guilherme-smarquees <guilhermesmarquees@MacBook-Air-de-Guilherme-2.local>
2025-05-09 21:20:32 -0300

    phone

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,48 +1,49 @@
-const products =  [
-    {price: 10.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
-    {price: 20.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
-    {price: 30.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
-    {price: 40.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
-    {price: 50.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
-    {price: 60.00, description: 'fogão', img: 'https://brastemp.vtexassets.com/arquivos/ids/229852/7891129530300_2.jpg?v=637973942954430000'},
-]
-
 async function getData(){
-    const data = await fetch()
+    const data = await fetch('assets/data.json')
     
-    console.log(await data.json())
+    const products = await data.json()
+
+    products.data.forEach(element => {
+        const newProd = document.createElement('div');
+        newProd.className = "product-container"
+        
+        const Prod = document.createElement('div');
+        Prod.className = "product"
+        
+        const img = document.createElement('img');
+        img.src = element.img
+        
+        const description = document.createElement('p');
+        description.className = "description"
+        description.textContent = element.description
+    
+        const price = document.createElement('p');
+        price.className = "price-tag"
+        price.textContent = "R$ " + element.price
+
+        const gift_icon = document.createElement('img');
+        gift_icon.className = "gift-icon"
+        gift_icon.src = "assets/img/gift-solid.svg"
+        
+        const button_text = document.createElement('p');
+        button_text.textContent = "Presentear"
+
+        const button = document.createElement('button');
+        button.addEventListener('click', (event) => {
+        })
+        
+        newProd.appendChild(Prod)
+        Prod.appendChild(img)
+        Prod.appendChild(description)
+        Prod.appendChild(price)
+        button.appendChild(gift_icon)
+        button.appendChild(button_text)
+        Prod.appendChild(button)
+        document.querySelector('.main-content').appendChild(newProd)
+    })
+    
+    
 }
 
-products.forEach(element => {
-    const newProd = document.createElement('div');
-    newProd.className = "product-container"
-    
-    const Prod = document.createElement('div');
-    Prod.className = "product"
-    
-    const img = document.createElement('img');
-    img.src = element.img
-    
-    const description = document.createElement('p');
-    description.className = "description"
-    description.textContent = element.description
-
-    const price = document.createElement('p');
-    price.className = "price-tag"
-    price.textContent = "R$ " + element.price
-    
-    const button = document.createElement('button');
-    button.textContent = "Presentear"
-    button.addEventListener('click', (event) => {
-        //window.location.replace(``)
-    })
-
-    newProd.appendChild(Prod)
-    Prod.appendChild(img)
-    Prod.appendChild(description)
-    Prod.appendChild(price)
-    Prod.appendChild(button)
-    document.querySelector('.main-content').appendChild(newProd)
-})
-
+const products = getData()
 
